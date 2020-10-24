35e0c797f0b9813e5ebd76c37bc14cd28b2cddc9
Miller <amiller@mpr.com>
2020-10-23 17:05:27 -0700

    pattagucci text

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 
 app.debug = True
 app.config.from_object(Config)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 from app import routes
--- app/static/css/a.py
+++
@@ -1,8 +0,0 @@
-def main():
-    x = 5
-    y = "afdsadf"
-    print("hello")
-    return x
-
-if __name__=="__main__":
-    main()
