86f5e5019ff674844dd86b704301caaf053d33d0
Al Miller <am@Als-MacBook-Pro.local>
2021-04-03 14:42:20 -0700

    fixed dark toggle

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-# <i>deconstruction</i> <a href="https://almiller.co/" target="__blank">(almiller.co)</a>
+# <a href="https://almiller.co/" target="__blank">(almiller.co)</a>
 
 ##  About
-npm install --save-dev eslint eslint-config-prettier eslint-plugin-prettier prettier eslint-plugin-vue eslint-plugin-prettier-vue
+
 <p>
- <i>deconstruction</i> <a href="https://almiller.co/" target="__blank">(almiller.co)</a> is a personal collection of technical and abstract articles.
+<a href="https://almiller.co/" target="__blank">(almiller.co)</a> is a collection of personal, technical and abstract articles.
 </p>
- The title is in reference to the work of French philosopher <a href="https://en.wikipedia.org/wiki/Jacques_Derrida" target="__blank">Jacques Derrida</a>, whose concept of deconstruction, in its most distilled form, provides a basis for questioning both our beliefs about the world and the integrity of the process through which we derive them.
 
 ##  Technical
 
 ###    Docsify.js
 
-<i>deconstruction</i> uses <a href="https://docsify.js.org/" target="__blank">docsify.js</a>, an open-source markdown documentation generator. The following docsify plugins are also used:
+This uses <a href="https://docsify.js.org/" target="__blank">docsify.js</a>, an open-source markdown documentation generator. The following docsify plugins are also used:
 
 - <a href="https://jhildenbiddle.github.io/docsify-themeable/#/" target="__blank">docsify-themeable</a>
 - <a href="https://github.com/erickjx/docsify-footer-enh" target="__blank">docsify-footer</a>
@@ -30,7 +29,12 @@ The following rewrites/redirects are used for the hosted domain <a href="https:/
 | -------- | ----------- | ------------- |
 | /<*>.md  | /<*>.md     | 200 (Rewrite) |
 | /<*>.jpg | /<*>.jpg    | 200 (Rewrite) |
+| /<*>.pdf | /<*>.pdf    | 200 (Rewrite) |
+| /<*>.js | /<*>.js    | 200 (Rewrite) |
+| /<*>.css | /<*>.css    | 200 (Rewrite) |
+| /<*>.svg | /<*>.svg    | 200 (Rewrite) |
 | /<*>     | /index.html | 200 (Rewrite) |
+| https://www.almiller.co    | https://almiller.co | 301 (Redirect) |
 
 ##  Usage
 
--- src/static/plugins/toggleTheme.js
+++ src/static/plugins/toggleTheme.js
@@ -5,10 +5,23 @@
 const plugin = (hook, vm) => {
 
   hook.doneEach(function () {
+
     if (document.getElementById('theme-toggle-button')) {
       document.getElementById('theme-toggle-button').remove();
-
     }
+
+    var el = document.createElement('input');
+    document.getElementById
+    document.getElementsByClassName('sidebar')[0].appendChild(el)
+
+    el.setAttribute('id', 'theme-toggle-button');
+    el.setAttribute('type', 'image');
+    el.setAttribute('src', '/static/assets/theme/moon.svg');
+    console.log(el)
+    document.getElementById("theme-toggle-button").addEventListener("click", function () {
+      toggleTheme();
+    });
+
     // console.log('plugins:', window.$docsify.plugins[0])
     function detectThemePreference() {
       var theme = 'light';
@@ -33,37 +46,33 @@ const plugin = (hook, vm) => {
     function toggleTheme() {
       console.log("toggle")
       if (document.documentElement.getAttribute('data-theme') == 'dark') {
-        setDarkTheme();
-      } else {
+        console.log('dark')
         setLightTheme();
+      } else {
+        console.log('light')
+        setDarkTheme();
       }
     }
     function setDarkTheme() {
-      // document.getElementById("theme-toggle-button").src = "./static/assets/theme/sun.svg";
+      if (document.getElementById("theme-toggle-button")) {
+        document.getElementById("theme-toggle-button").src = "./static/assets/theme/sun.svg";
+      }
       document.getElementById('circles-animation').src = './static/assets/theme/circles_dark.svg';
       document.documentElement.setAttribute('data-theme', 'dark')
       localStorage.setItem('theme', 'dark');
     }
-  
+
     function setLightTheme() {
-      // document.getElementById("theme-toggle-button").src = "./static/assets/theme/moon.svg";
+      if (document.getElementById("theme-toggle-button")) {
+        document.getElementById("theme-toggle-button").src = "./static/assets/theme/moon.svg";
+      }
       document.getElementById('circles-animation').src = './static/assets/theme/circles.svg';
       document.documentElement.setAttribute('data-theme', 'light')
       localStorage.setItem('theme', 'light');
     }
     detectThemePreference();
-    var el = document.createElement('input');
-    document.body.appendChild(el);
 
-    el.setAttribute('id', 'theme-toggle-button');
-    el.setAttribute('type', 'image');
-    el.setAttribute('src', '/static/assets/theme/moon.svg');
-    el.addEventListener("click", toggleTheme());
-    console.log(el)
-    // document.getElementById("theme-toggle-button").addEventListener("click", () => { console.log("asdf") });
-
-  });
-
+  })
 }
 
 window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins)
